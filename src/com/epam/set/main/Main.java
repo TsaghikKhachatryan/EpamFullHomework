b6c7e46346df11f8a462064687538db05f0a4edb
TsaghikKhachatryan <khachatryan.tsaghik@gmail.com>
2020-02-05 19:29:03 +0400

    test functions have been added

--- src/com/epam/set/main/Main.java
+++ src/com/epam/set/main/Main.java
@@ -1,7 +1,24 @@
 package com.epam.set.main;
 
+import com.epam.set.model.Coffee;
+import com.epam.set.model.Flower;
+import com.epam.set.model.Student;
+import com.epam.set.utils.MySet;
+import com.epam.set.utils.SetHelper;
+
 public class Main {
     public static void main(String[] args) {
-
+        manageMySet();
+    }
+    private static void manageMySet() {
+        MySet<Student> myStudentSet= SetHelper.createStudentsSet();
+        MySet<Coffee> myCoffeeSet= SetHelper.createCoffeeSet();
+        MySet<Flower> myFlowerSet= SetHelper.createFlowerSet();
+        System.out.println("Student's Set");
+        SetHelper.printSet(myStudentSet);
+        System.out.println("Coffe's Set");
+        SetHelper.printSet(myCoffeeSet);
+        System.out.println("Flower's Set");
+        SetHelper.printSet(myFlowerSet);
     }
 }
