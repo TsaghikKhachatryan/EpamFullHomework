2f6b0e8e31fa656ee6f74dd1d7cd264e59ba3fac
TsaghikKhachatryan <khachatryan.tsaghik@gmail.com>
2020-02-05 19:47:30 +0400

    dynamic array have been changed,simple array added instead of

--- src/com/epam/months/models/Months.java
+++ src/com/epam/months/models/Months.java
@@ -1,7 +1,5 @@
 package com.epam.months.models;
 
-import java.util.ArrayList;
-
 public enum Months {
     JANUARY(31, new int[]{1, 6, 13, 31}),
     FEBRUARY(28, new int[]{5, 23, 26}),
@@ -14,7 +12,7 @@ public enum Months {
     SEPTEMBER(30, new int[]{1, 21}),
     OCTOBER(31, new int[]{12}),
     NOVEMBER(30, new int[]{6, 18}),
-    DECEMBER(31, , new int[]{1, 7, 31});
+    DECEMBER(31, new int[]{1, 7, 31});
 
     private final int DAYS_COUNT;
     private final int[] HLOIDAYS_LIST;
