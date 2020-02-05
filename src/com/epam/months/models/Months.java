688559ea77c4873ea101441e073c7f7adb1574d1
TsaghikKhachatryan <khachatryan.tsaghik@gmail.com>
2020-02-05 19:43:04 +0400

    dynamic array have been changed,simple array added instead of

--- src/com/epam/months/models/Months.java
+++ src/com/epam/months/models/Months.java
@@ -3,28 +3,28 @@ package com.epam.months.models;
 import java.util.ArrayList;
 
 public enum Months {
-    JANUARY(31,new ArrayList<>(){{add(1);add(6);add(13);}}),
-    FEBRUARY(28,new ArrayList<>(){{add(23);}}),
-    MARCH(31,new ArrayList<>(){{add(8);add(22);add(24);}}),
-    APRIL(30,new ArrayList<>(){{add(7);add(24);}}),
-    MAY(31,new ArrayList<>(){{add(1);add(9);add(28);}}),
-    JUNE(30,new ArrayList<>(){{add(1);add(7);}}),
-    JULY(31,new ArrayList<>(){{add(7);add(23);add(14);}}),
-    AUGUST(31,new ArrayList<>(){{add(10);add(20);add(26);}}),
-    SEPTEMBER(30,new ArrayList<>(){{add(1);add(7);add(24);}}),
-    OCTOBER(31,new ArrayList<>(){{add(24);}}),
-    NOVEMBER(30,new ArrayList<>(){{add(14);}}),
-    DECEMBER(31,new ArrayList<>(){{add(1);add(15);add(13);}});
+    JANUARY(31, new int[]{1, 6, 13, 31}),
+    FEBRUARY(28, new int[]{5, 23, 26}),
+    MARCH(31, new int[]{8, 12, 13}),
+    APRIL(30, new int[]{7, 15, 24}),
+    MAY(31, new int[]{1, 9, 28}),
+    JUNE(30, new int[]{1, 7}),
+    JULY(31, new int[]{2, 13}),
+    AUGUST(31, new int[]{10, 20, 26, 31}),
+    SEPTEMBER(30, new int[]{1, 21}),
+    OCTOBER(31, new int[]{12}),
+    NOVEMBER(30, new int[]{6, 18}),
+    DECEMBER(31, , new int[]{1, 7, 31});
 
     private final int DAYS_COUNT;
-    private final ArrayList<Integer> HLOIDAYS_LIST;
+    private final int[] HLOIDAYS_LIST;
 
-    Months(int DAYS_COUNT, ArrayList<Integer> HLOIDAYS_LIST) {
+    Months(int DAYS_COUNT, int[] HLOIDAYS_LIST) {
         this.DAYS_COUNT = DAYS_COUNT;
         this.HLOIDAYS_LIST = HLOIDAYS_LIST;
     }
 
-    public ArrayList<Integer> getHLOIDAYS_LIST() {
+    public int[] getHLOIDAYS_LIST() {
         return HLOIDAYS_LIST;
     }
 
