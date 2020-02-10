27bf5cb08e61da9216032a679c07e8133f06a99f
Student <Student@EPAMYERW0036.moscow.epam.com>
2020-02-10 17:08:06 +0400

    javaDoc have been edited

--- src/com/epam/set/utils/SetHelper.java
+++ src/com/epam/set/utils/SetHelper.java
@@ -39,43 +39,4 @@ public class SetHelper {
         myFlowerLinkedSet.add(new Flower("Lily", "pink"));
         return myFlowerLinkedSet;
     }
-
-    /*//Creating Sets for MySet
-    public static MySet<Student> createStudentsSet() {
-        MySet<Student> myStudentSet = new MySet<>();
-        myStudentSet.add(new Student("Sargis", "Sargsyan", 20));
-        myStudentSet.add(new Student("Ani", "Sargsyan", 80));
-        myStudentSet.add(new Student("Ani", "Sargsyan", 27));
-        myStudentSet.add(new Student("Poghos", "Poghosyan", 21));
-        myStudentSet.add(new Student("Poghos", "Poghosyan", 21));
-        myStudentSet.add(new Student("Sargis", "Meloyan", 25));
-        myStudentSet.add(new Student("Anna", "Mnoyan", 30));
-        return myStudentSet;
-    }
-
-    public static MySet<Flower> createFlowerSet() {
-        MySet<Flower> myFlowerSet = new MySet<>();
-        myFlowerSet.add(new Flower("Rose", "white"));
-        myFlowerSet.add(new Flower("Lily", "yellow"));
-        myFlowerSet.add(new Flower("Rose", "red"));
-        myFlowerSet.add(new Flower("Orchids", "blue"));
-        myFlowerSet.add(new Flower("Lily", "pink"));
-        return myFlowerSet;
-    }
-
-    public static MySet<Coffee> createCoffeeSet() {
-        MySet<Coffee> myCoffeeSet = new MySet<>();
-        myCoffeeSet.add(new Coffee("black", "Torabika", "Africa"));
-        myCoffeeSet.add(new Coffee("black", "Arabica", "America"));
-        myCoffeeSet.add(new Coffee("brown", "Torabika", "China"));
-        myCoffeeSet.add(new Coffee("darkbrown", "Arabica", "Brazil"));
-        return myCoffeeSet;
-    }
-
-    public static void printSet(MySet mySet) {
-        for (Object o : mySet) {
-            System.out.println(o);
-        }
-        System.out.println();
-    }*/
 }
